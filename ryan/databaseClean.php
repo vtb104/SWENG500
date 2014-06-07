e0c9a2dedb5c5817f41d2ca986e5c34c5aaa1643
ryaneyoung <ryanessonyoung@gmail.com>
2014-06-07 01:09:21 -0800

    Added my folder
    PHP test cases are done for the database file.

---
+++ ryan/databaseClean.php
@@ -0,0 +1,11 @@
+<?php
+//Cleans the database of test points
+require_once("database.php");
+require_once("vars.php");
+
+$db = new MySQLi('localhost', _DB_USERNAME, _DB_PASSWORD, _DB_NAME) or die (mysqli_error() . "Database Error 1");
+$query = "DELETE FROM Points WHERE pointNotes = 'Demo Point'";
+echo $db->query($query);
+
+
+?>
