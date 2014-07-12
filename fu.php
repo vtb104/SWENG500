abc97f682d3cfc55bc9bf2517f731ec6c92f7542
Shane Halse <shanehalse@gmail.com>
2014-07-11 21:38:50 -0400

    Css file, user return for login
    Moved styling to a css file
    Can now hit "return" or "enter" when logging in instead of having to
    click "Login button"

--- fu.php
+++ fu.php
@@ -12,18 +12,7 @@
   <link rel="icon" type="image/png" href="favicon.ico"/>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css">
-  <style>
-    #map_canvas{
-	position: absolute;
-	left: 0px;
-	bottom: 0px;
-	height: 50%;
-	width: 100%;
-	z-index: 10;
-        margin: 0;
-        padding: 0;
-}
-  </style>
+    <link rel="stylesheet" type="text/css" href="fu.css">
   <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
   <script src="https://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script>
   <script src="https://maps.googleapis.com/maps/api/js?sensor=true"></script>
